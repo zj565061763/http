f90d7135d8407d91191afbc558d3d351531a7508
zjun <565061763@qq.com>
2017-10-10 12:05:30 +0800

    demo测试

--- app/src/main/java/com/fanwe/www/http/MainActivity.java
+++ app/src/main/java/com/fanwe/www/http/MainActivity.java
@@ -1,15 +1,24 @@
 package com.fanwe.www.http;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.fanwe.lib.http.SDHttpRequest;
 
 public class MainActivity extends AppCompatActivity
 {
+    public static final String URL = "http://ilvbt3.fanwe.net/mapi/index.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        SDHttpRequest.post(URL)
+                .form("ctl", "app")
+                .form("act", "init");
+
     }
 }
