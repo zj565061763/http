459edba6fdd0457b9ed1815f5b281724d6b5eb3e
zjun <565061763@qq.com>
2017-10-18 16:36:20 +0800

    demo测试

--- app/src/main/java/com/fanwe/www/http/SyncRequestActivity.java
+++ app/src/main/java/com/fanwe/www/http/SyncRequestActivity.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.View;
 
 import com.fanwe.lib.http.PostRequest;
+import com.fanwe.lib.http.Request;
 import com.fanwe.lib.http.Response;
 
 import java.io.InputStream;
@@ -35,9 +36,9 @@ public class SyncRequestActivity extends AppCompatActivity
             {
                 try
                 {
-                    Response response = new PostRequest(URL).param("ctl", "app").param("act", "init")
-                            .setTag(this)
-                            .execute();
+                    Request request = new PostRequest(URL); //创建请求对象
+                    request.param("ctl", "app").param("act", "init"); //创建要提交的form数据
+                    Response response = request.execute(); //发起请求，得到Response对象
 
                     InputStream inputStream = response.getInputStream(); //结果以输入流返回
                     int code = response.getCode(); //返回码
