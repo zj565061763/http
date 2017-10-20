e62f41d8c435c59b3f2c75b585490a2b2132a8c3
zjun <565061763@qq.com>
2017-10-20 17:25:22 +0800

    重构回调代理

--- lib/src/main/java/com/fanwe/lib/http/callback/RequestCallbackProxy.java
+++ lib/src/main/java/com/fanwe/lib/http/callback/RequestCallbackProxy.java
@@ -1,5 +1,9 @@
 package com.fanwe.lib.http.callback;
 
+import com.fanwe.lib.http.Request;
+import com.fanwe.lib.http.Response;
+import com.fanwe.lib.http.utils.TransmitParam;
+
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
@@ -7,13 +11,21 @@ import java.lang.reflect.Proxy;
 /**
  * Created by zhengjun on 2017/10/12.
  */
-public final class RequestCallbackProxy
+public final class RequestCallbackProxy implements IRequestCallback
 {
-    private RequestCallbackProxy()
+    private IRequestCallback[] mArrCallback;
+
+    private RequestCallbackProxy(IRequestCallback... callbacks)
     {
+        mArrCallback = callbacks;
     }
 
     public static IRequestCallback get(final IRequestCallback... callbacks)
+    {
+        return new RequestCallbackProxy(callbacks);
+    }
+
+    public static IRequestCallback getProxy(final IRequestCallback... callbacks)
     {
         if (callbacks == null || callbacks.length <= 0)
         {
@@ -40,5 +52,168 @@ public final class RequestCallbackProxy
                 });
 
         return (IRequestCallback) object;
+    }
+
+    public IRequestCallback[] getArrCallback()
+    {
+        if (mArrCallback == null)
+        {
+            mArrCallback = new IRequestCallback[0];
+        }
+        return mArrCallback;
+    }
+
+    @Override
+    public void setRequest(Request request)
+    {
+        for (IRequestCallback item : getArrCallback())
+        {
+            if (item != null)
+            {
+                item.setRequest(request);
+            }
+        }
+    }
+
+    @Override
+    public Request getRequest()
+    {
+        int length = getArrCallback().length;
+        if (length > 0)
+        {
+            return getArrCallback()[length - 1].getRequest();
+        }
+        return null;
+    }
+
+    @Override
+    public void setResponse(Response response)
+    {
+        for (IRequestCallback item : getArrCallback())
+        {
+            if (item != null)
+            {
+                item.setResponse(response);
+            }
+        }
+    }
+
+    @Override
+    public Response getResponse()
+    {
+        int length = getArrCallback().length;
+        if (length > 0)
+        {
+            return getArrCallback()[length - 1].getResponse();
+        }
+        return null;
+    }
+
+    @Override
+    public void onPrepare(Request request)
+    {
+        for (IRequestCallback item : getArrCallback())
+        {
+            if (item != null)
+            {
+                item.onPrepare(request);
+            }
+        }
+    }
+
+    @Override
+    public void onStart()
+    {
+        for (IRequestCallback item : getArrCallback())
+        {
+            if (item != null)
+            {
+                item.onStart();
+            }
+        }
+    }
+
+    @Override
+    public void onSuccessBackground() throws Exception
+    {
+        for (IRequestCallback item : getArrCallback())
+        {
+            if (item != null)
+            {
+                item.onSuccessBackground();
+            }
+        }
+    }
+
+    @Override
+    public void onSuccessBefore()
+    {
+        for (IRequestCallback item : getArrCallback())
+        {
+            if (item != null)
+            {
+                item.onSuccessBefore();
+            }
+        }
+    }
+
+    @Override
+    public void onSuccess()
+    {
+        for (IRequestCallback item : getArrCallback())
+        {
+            if (item != null)
+            {
+                item.onSuccess();
+            }
+        }
+    }
+
+    @Override
+    public void onError(Exception e)
+    {
+        for (IRequestCallback item : getArrCallback())
+        {
+            if (item != null)
+            {
+                item.onError(e);
+            }
+        }
+    }
+
+    @Override
+    public void onCancel()
+    {
+        for (IRequestCallback item : getArrCallback())
+        {
+            if (item != null)
+            {
+                item.onCancel();
+            }
+        }
+    }
+
+    @Override
+    public void onFinish()
+    {
+        for (IRequestCallback item : getArrCallback())
+        {
+            if (item != null)
+            {
+                item.onFinish();
+            }
+        }
+    }
+
+    @Override
+    public void onProgressUpload(TransmitParam param)
+    {
+        for (IRequestCallback item : getArrCallback())
+        {
+            if (item != null)
+            {
+                item.onProgressUpload(param);
+            }
+        }
     }
 }
