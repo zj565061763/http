676d906577a02b917d8bbde409941b4bc5a0b8db
zjun <565061763@qq.com>
2018-03-14 10:28:44 +0800

    重构

--- lib/src/main/java/com/fanwe/lib/http/IPostRequest.java
+++ lib/src/main/java/com/fanwe/lib/http/IPostRequest.java
@@ -1,8 +1,13 @@
 package com.fanwe.lib.http;
 
+import java.io.File;
+
 /**
  * Created by zhengjun on 2018/3/13.
  */
 public interface IPostRequest extends IRequest
 {
+    IPostRequest addFile(String name, File file);
+
+    IPostRequest addFile(String name, String filename, String contentType, File file);
 }
