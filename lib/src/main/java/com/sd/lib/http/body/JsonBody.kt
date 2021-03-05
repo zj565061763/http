f4ddde9265c4a2ea8534f2303ef5836377a03cd0
zjun <565061763@qq.com>
2021-03-05 11:28:59 +0800

    kotlin JsonBody

--- lib/src/main/java/com/sd/lib/http/body/JsonBody.kt
+++ lib/src/main/java/com/sd/lib/http/body/JsonBody.kt
@@ -1,18 +1,6 @@
-package com.sd.lib.http.body;
+package com.sd.lib.http.body
 
-import com.sd.lib.http.ContentType;
+import com.sd.lib.http.ContentType
+import java.nio.charset.Charset
 
-import java.nio.charset.Charset;
-
-public class JsonBody extends StringBody
-{
-    public JsonBody(String body)
-    {
-        this(body, null);
-    }
-
-    public JsonBody(String body, Charset charset)
-    {
-        super(body, charset, ContentType.JSON);
-    }
-}
+class JsonBody @JvmOverloads constructor(body: String?, charset: Charset? = null) : StringBody(body, charset, ContentType.JSON)
