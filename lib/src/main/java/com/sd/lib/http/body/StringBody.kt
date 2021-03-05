304684c1b0b55576a9a6a198083087c6d71287d6
zjun <565061763@qq.com>
2021-03-05 11:24:02 +0800

    kotlin StringBody

--- lib/src/main/java/com/sd/lib/http/body/StringBody.kt
+++ lib/src/main/java/com/sd/lib/http/body/StringBody.kt
@@ -1,37 +1,10 @@
-package com.sd.lib.http.body;
+package com.sd.lib.http.body
 
-import android.text.TextUtils;
+import android.text.TextUtils
+import com.sd.lib.http.ContentType
+import java.nio.charset.Charset
 
-import com.sd.lib.http.ContentType;
-
-import java.nio.charset.Charset;
-
-public class StringBody extends CharsetBody<String>
-{
-    private final String mBody;
-    private final String mContentType;
-
-    public StringBody(String body, String contentType)
-    {
-        this(body, null, contentType);
-    }
-
-    public StringBody(String body, Charset charset, String contentType)
-    {
-        super(charset);
-        mBody = TextUtils.isEmpty(body) ? "" : body;
-        mContentType = TextUtils.isEmpty(contentType) ? ContentType.STREAM : contentType;
-    }
-
-    @Override
-    public final String getContentType()
-    {
-        return mContentType;
-    }
-
-    @Override
-    public final String getBody()
-    {
-        return mBody;
-    }
+open class StringBody(body: String?, charset: Charset? = null, contentType: String? = null) : CharsetBody<String?>(charset) {
+    override val contentType: String = if (TextUtils.isEmpty(contentType)) ContentType.STREAM else contentType!!
+    override val body: String = if (TextUtils.isEmpty(body)) "" else body!!
 }
