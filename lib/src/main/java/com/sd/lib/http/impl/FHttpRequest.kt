5396146d2405eec63101e377560ddd7c2838e2e6
zjun <565061763@qq.com>
2021-03-05 17:08:37 +0800

    kotlin FHttpRequest

--- lib/src/main/java/com/sd/lib/http/impl/FHttpRequest.kt
+++ lib/src/main/java/com/sd/lib/http/impl/FHttpRequest.kt
@@ -1,109 +1,85 @@
-package com.sd.lib.http.impl;
+package com.sd.lib.http.impl
 
-import android.text.TextUtils;
+import android.text.TextUtils
+import com.sd.lib.http.RequestManager
+import com.sd.lib.http.utils.HttpLog
+import java.net.HttpCookie
+import java.util.*
 
-import com.sd.lib.http.RequestManager;
-import com.sd.lib.http.utils.HttpLog;
-
-import java.net.HttpCookie;
-import java.net.URI;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
-class FHttpRequest extends HttpRequest
-{
-    public FHttpRequest(CharSequence url, String method) throws HttpRequestException
-    {
-        super(url, method);
-        loadCookieForRequest();
+internal class FHttpRequest : HttpRequest {
+    constructor(url: CharSequence?, method: String?) : super(url, method) {
+        loadCookieForRequest()
     }
 
-    public FHttpRequest(URL url, String method) throws HttpRequestException
-    {
-        super(url, method);
-        loadCookieForRequest();
-    }
-
-    /**
-     * 'Set-Cookie' header name
-     */
-    public static final String HEADER_SET_COOKIE = "Set-Cookie";
-    /**
-     * 'Cookie' header name
-     */
-    public static final String HEADER_COOKIE = "Cookie";
-
-    private Integer mCode = null;
-
-    private List<HttpCookie> getResponseCookie()
-    {
-        final Map<String, List<String>> headers = headers();
-        if (headers == null || headers.isEmpty())
-            return null;
-
-        final List<String> listCookie = headers.get(HEADER_SET_COOKIE);
-        if (listCookie == null || listCookie.isEmpty())
-            return null;
-
-        HttpLog.i("cookie ---------->saveCookieFromResponse " + url() + "\r\n" + TextUtils.join("\r\n", listCookie));
-        final List<HttpCookie> listResult = new ArrayList<>();
-        for (String item : listCookie)
-        {
-            listResult.addAll(HttpCookie.parse(item));
-        }
-        return listResult;
-    }
-
-    private void loadCookieForRequest()
-    {
-        try
-        {
-            final URI uri = url().toURI();
-            final List<HttpCookie> listCookie = RequestManager.getInstance().getCookieStore().get(uri);
-
-            if (listCookie != null && !listCookie.isEmpty())
-            {
-                final String cookie = TextUtils.join(";", listCookie);
-                header(HEADER_COOKIE, cookie);
-                HttpLog.i("cookie loadCookieForRequest " + url() + "\r\n" + cookie);
+    private var mCode: Int? = null
+    private val responseCookie: List<HttpCookie>?
+        private get() {
+            val headers = headers()
+            if (headers == null || headers.isEmpty()) return null
+            val listCookie = headers[HEADER_SET_COOKIE]
+            if (listCookie == null || listCookie.isEmpty()) return null
+            HttpLog.i("""
+    cookie ---------->saveCookieFromResponse ${url()}
+    ${TextUtils.join("\r\n", listCookie)}
+    """.trimIndent())
+            val listResult: MutableList<HttpCookie> = ArrayList()
+            for (item in listCookie) {
+                listResult.addAll(HttpCookie.parse(item))
             }
-        } catch (Exception e)
-        {
-            HttpLog.e("cookie loadCookieForRequest error:" + e);
+            return listResult
+        }
+
+    /**
+     * 填充cookie到请求对象
+     */
+    private fun loadCookieForRequest() {
+        try {
+            val uri = url().toURI()
+            val listCookie = RequestManager.getInstance().cookieStore[uri]
+            if (listCookie != null && !listCookie.isEmpty()) {
+                val cookie = TextUtils.join(";", listCookie)
+                header(HEADER_COOKIE, cookie)
+                HttpLog.i("""cookie loadCookieForRequest $uri
+                    |$cookie
+                """.trimMargin())
+            }
+        } catch (e: Exception) {
+            HttpLog.e("cookie loadCookieForRequest error:$e")
         }
     }
 
-    private void saveCookieFromResponse()
-    {
-        try
-        {
-            final URI uri = url().toURI();
-            final List<HttpCookie> listCookie = getResponseCookie();
-
-            RequestManager.getInstance().getCookieStore().add(uri, listCookie);
-        } catch (Exception e)
-        {
-            HttpLog.e("cookie saveCookieFromResponse error:" + e);
+    /**
+     * 保存接口返回的cookie
+     */
+    private fun saveCookieFromResponse() {
+        try {
+            val uri = url().toURI()
+            val listCookie = responseCookie
+            RequestManager.getInstance().cookieStore.add(uri, listCookie)
+        } catch (e: Exception) {
+            HttpLog.e("cookie saveCookieFromResponse error:$e")
         }
     }
 
     //---------- Override start ----------
 
-    @Override
-    public int code() throws HttpRequestException
-    {
-        final int code = super.code();
-
-        if (mCode == null)
-        {
-            mCode = code;
-            saveCookieFromResponse();
+    @Throws(HttpRequestException::class)
+    override fun code(): Int {
+        val code = super.code()
+        if (mCode == null) {
+            mCode = code
+            saveCookieFromResponse()
         }
-
-        return code;
+        return code
     }
 
     //---------- Override end ----------
+
+    companion object {
+        /** 'Set-Cookie' header name */
+        const val HEADER_SET_COOKIE = "Set-Cookie"
+
+        /** 'Cookie' header name */
+        const val HEADER_COOKIE = "Cookie"
+    }
 }
