57ac65c7f656675f6cef53f693d4beb88b373994
zjun <565061763@qq.com>
2021-03-05 15:38:15 +0800

    kotlin IResponse

--- lib/src/main/java/com/sd/lib/http/IResponse.kt
+++ lib/src/main/java/com/sd/lib/http/IResponse.kt
@@ -1,32 +1,38 @@
-package com.sd.lib.http;
+package com.sd.lib.http
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.List;
-import java.util.Map;
+import java.io.IOException
+import java.io.InputStream
 
-public interface IResponse
-{
+interface IResponse {
     /**
      * http返回码
-     *
-     * @return
      */
-    int getCode();
-
-    int getContentLength();
-
-    Map<String, List<String>> getHeaders();
-
-    String getCharset();
-
-    InputStream getInputStream();
+    @get:Throws(IOException::class)
+    val code: Int
 
     /**
-     * 读取输入流的内容，并以字符串返回(必须在非UI线程调用)
-     *
-     * @return
-     * @throws IOException
+     * 内容长度
      */
-    String getAsString() throws IOException;
+    val contentLength: Int
+
+    /**
+     * 头信息
+     */
+    val headers: Map<String, List<String>>
+
+    /**
+     * 编码格式
+     */
+    val charset: String?
+
+    /**
+     * 输入流
+     */
+    val inputStream: InputStream
+
+    /**
+     * 读取输入流的内容，并以字符串返回（必须在非UI线程调用）
+     */
+    @get:Throws(IOException::class)
+    val asString: String?
 }
