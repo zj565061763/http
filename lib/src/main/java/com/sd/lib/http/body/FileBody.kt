6036fe76b98903a869c2a462c4f08e01f49af270
zjun <565061763@qq.com>
2021-03-05 11:17:10 +0800

    kotlin FileBody

--- lib/src/main/java/com/sd/lib/http/body/FileBody.kt
+++ lib/src/main/java/com/sd/lib/http/body/FileBody.kt
@@ -1,36 +1,20 @@
-package com.sd.lib.http.body;
+package com.sd.lib.http.body
 
-import android.text.TextUtils;
+import android.text.TextUtils
+import com.sd.lib.http.ContentType
+import java.io.File
+import java.net.HttpURLConnection
 
-import com.sd.lib.http.ContentType;
+class FileBody(file: File) : IRequestBody<File> {
 
-import java.io.File;
-import java.net.HttpURLConnection;
+    override val contentType: String
+        get() {
+            var contentType = HttpURLConnection.guessContentTypeFromName(body.name)
+            if (TextUtils.isEmpty(contentType)) {
+                contentType = ContentType.STREAM
+            }
+            return contentType
+        }
 
-public class FileBody implements IRequestBody<File>
-{
-    private final File mFile;
-
-    public FileBody(File file)
-    {
-        if (file == null)
-            throw new NullPointerException("file is null");
-        mFile = file;
-    }
-
-    @Override
-    public final String getContentType()
-    {
-        String contentType = HttpURLConnection.guessContentTypeFromName(mFile.getName());
-        if (TextUtils.isEmpty(contentType))
-            contentType = ContentType.STREAM;
-
-        return contentType;
-    }
-
-    @Override
-    public final File getBody()
-    {
-        return mFile;
-    }
+    override val body: File = file
 }
