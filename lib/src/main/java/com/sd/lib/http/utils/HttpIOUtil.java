23ffef2144b3f337ec6d8ec4b76875a4c5572f3e
zjun <565061763@qq.com>
2021-03-05 17:53:19 +0800

    HttpIOUtil

--- lib/src/main/java/com/sd/lib/http/utils/HttpIOUtil.java
+++ lib/src/main/java/com/sd/lib/http/utils/HttpIOUtil.java
@@ -19,15 +19,15 @@ public class HttpIOUtil
     {
     }
 
-    public static String readString(InputStream in, String charset) throws IOException
+    public static String readString(InputStream inputStream, String charset) throws IOException
     {
         if (TextUtils.isEmpty(charset))
             charset = "UTF-8";
 
-        if (!(in instanceof BufferedInputStream))
-            in = new BufferedInputStream(in);
+        if (!(inputStream instanceof BufferedInputStream))
+            inputStream = new BufferedInputStream(inputStream);
 
-        final Reader reader = new InputStreamReader(in, charset);
+        final Reader reader = new InputStreamReader(inputStream, charset);
         final StringBuilder sb = new StringBuilder();
         final char[] buffer = new char[1024];
         int len;
@@ -38,36 +38,36 @@ public class HttpIOUtil
         return sb.toString();
     }
 
-    public static void writeString(OutputStream out, String content, String charset) throws IOException
+    public static void writeString(OutputStream outputStream, String content, String charset) throws IOException
     {
         if (TextUtils.isEmpty(charset))
             charset = "UTF-8";
 
-        final Writer writer = new OutputStreamWriter(out, charset);
+        final Writer writer = new OutputStreamWriter(outputStream, charset);
         writer.write(content);
         writer.flush();
     }
 
-    public static void copy(InputStream in, OutputStream out, ProgressCallback callback) throws IOException
+    public static void copy(InputStream inputStream, OutputStream outputStream, ProgressCallback callback) throws IOException
     {
-        if (!(in instanceof BufferedInputStream))
-            in = new BufferedInputStream(in);
+        if (!(inputStream instanceof BufferedInputStream))
+            inputStream = new BufferedInputStream(inputStream);
 
-        if (!(out instanceof BufferedOutputStream))
-            out = new BufferedOutputStream(out);
+        if (!(outputStream instanceof BufferedOutputStream))
+            outputStream = new BufferedOutputStream(outputStream);
 
         long count = 0;
         int len = 0;
         final byte[] buffer = new byte[1024];
-        while ((len = in.read(buffer)) != -1)
+        while ((len = inputStream.read(buffer)) != -1)
         {
-            out.write(buffer, 0, len);
+            outputStream.write(buffer, 0, len);
             count += len;
 
             if (callback != null)
                 callback.onProgress(count);
         }
-        out.flush();
+        outputStream.flush();
     }
 
     public static void closeQuietly(Closeable closeable)
