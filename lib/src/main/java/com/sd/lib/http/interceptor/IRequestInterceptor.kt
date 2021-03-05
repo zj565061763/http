f5d43a2df1b6fb435513a3332fa99d43cdd93289
zjun <565061763@qq.com>
2021-03-05 12:05:29 +0800

    IRequestInterceptor

--- lib/src/main/java/com/sd/lib/http/interceptor/IRequestInterceptor.kt
+++ lib/src/main/java/com/sd/lib/http/interceptor/IRequestInterceptor.kt
@@ -1,36 +1,33 @@
-package com.sd.lib.http.interceptor;
+package com.sd.lib.http.interceptor
 
-import com.sd.lib.http.IRequest;
-import com.sd.lib.http.IResponse;
+import com.sd.lib.http.IRequest
+import com.sd.lib.http.IResponse
 
 /**
  * 用于请求拦截
  */
-public interface IRequestInterceptor
-{
+interface IRequestInterceptor {
     /**
      * 发起网络请求之前回调
      *
      * @param request
-     * @return 不为null，则用此方法的返回值
-     * @throws Exception
+     * @return 如果返回不为null，则使用该返回值
      */
-    IResponse beforeExecute(IRequest request) throws Exception;
+    @Throws(Exception::class)
+    fun beforeExecute(request: IRequest): IResponse?
 
     /**
      * 发起网络请求之后回调
      *
      * @param request
-     * @param response
-     * @return 不为null，则用此方法的返回值
-     * @throws Exception
+     * @param response 原始的返回
+     * @return 如果返回不为null，则使用该返回值
      */
-    IResponse afterExecute(IRequest request, IResponse response) throws Exception;
+    @Throws(Exception::class)
+    fun afterExecute(request: IRequest, response: IResponse): IResponse?
 
     /**
      * 异常回调
-     *
-     * @param e
      */
-    void onError(Exception e);
+    fun onError(e: Exception)
 }
