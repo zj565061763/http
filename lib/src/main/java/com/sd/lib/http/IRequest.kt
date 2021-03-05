e7ecb775a1486de4eae6f371a6084721d7224ea6
zjun <565061763@qq.com>
2021-03-05 15:20:22 +0800

    kotlin Request

--- lib/src/main/java/com/sd/lib/http/IRequest.kt
+++ lib/src/main/java/com/sd/lib/http/IRequest.kt
@@ -1,112 +1,88 @@
-package com.sd.lib.http;
+package com.sd.lib.http
 
-import com.sd.lib.http.callback.IUploadProgressCallback;
-import com.sd.lib.http.callback.RequestCallback;
-import com.sd.lib.http.utils.HttpDataHolder;
+import com.sd.lib.http.callback.IUploadProgressCallback
+import com.sd.lib.http.callback.RequestCallback
+import com.sd.lib.http.utils.HttpDataHolder
+import javax.net.ssl.HostnameVerifier
+import javax.net.ssl.SSLSocketFactory
 
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.SSLSocketFactory;
-
-public interface IRequest
-{
-    /**
-     * 默认连接超时
-     */
-    int DEFAULT_CONNECT_TIMEOUT = 15 * 1000;
-    /**
-     * 默认读取超时
-     */
-    int DEFAULT_READ_TIMEOUT = 15 * 1000;
+interface IRequest {
 
     /**
      * 设置基础url
      */
-    void setBaseUrl(String baseUrl);
+    var baseUrl: String
 
     /**
-     * 设置url地址的后缀字符串
+     * url地址的后缀字符串
      */
-    void setUrlSuffix(String urlSuffix);
+    var urlSuffix: String
+
+    /**
+     * [baseUrl] 和 [urlSuffix] 组拼后的url
+     */
+    val url: String
 
     /**
      * 设置请求标识
      */
-    void setTag(String tag);
+    var tag: String?
 
     /**
-     * 设置请求连接超时（毫秒），默认值{@link #DEFAULT_CONNECT_TIMEOUT}
+     * 连接超时（毫秒），默认值[DEFAULT_CONNECT_TIMEOUT]
      */
-    void setConnectTimeout(int connectTimeout);
+    var connectTimeout: Int
 
     /**
-     * 设置请求读取超时（毫秒），默认值{@link #DEFAULT_READ_TIMEOUT}
+     * 读取超时（毫秒），默认值[DEFAULT_READ_TIMEOUT]
      */
-    void setReadTimeout(int readTimeout);
-
-    /**
-     * 设置上传回调
-     */
-    void setUploadProgressCallback(IUploadProgressCallback callback);
-
-    void setSSLSocketFactory(SSLSocketFactory SSLSocketFactory);
-
-    void setHostnameVerifier(HostnameVerifier hostnameVerifier);
-
-    void setInterceptExecute(boolean intercept);
+    var readTimeout: Int
 
     /**
      * 返回设置的请求参数
      */
-    HttpDataHolder<String, Object> getParams();
+    val params: HttpDataHolder<String, Any>
 
     /**
      * 返回设置的Header
      */
-    HttpDataHolder<String, String> getHeaders();
+    val headers: HttpDataHolder<String, String>
 
     /**
-     * 返回设置的基础url{@link #setBaseUrl(String)}
+     * 是否拦截请求
      */
-    String getBaseUrl();
+    var interceptExecute: Boolean
 
     /**
-     * 返回设置的url后缀{@link #setUrlSuffix(String)}
+     * 上传回调
      */
-    String getUrlSuffix();
+    var uploadProgressCallback: IUploadProgressCallback?
 
-    /**
-     * 返回组拼后的基础url<br>
-     * {@link #setBaseUrl(String)} 和 {@link #setUrlSuffix(String)} 组拼成的
-     */
-    String getUrl();
+    var sSLSocketFactory: SSLSocketFactory?
 
-    /**
-     * 返回设置的标识{@link #setTag(String)}
-     */
-    String getTag();
-
-    SSLSocketFactory getSSLSocketFactory();
-
-    HostnameVerifier getHostnameVerifier();
+    var hostnameVerifier: HostnameVerifier?
 
     /**
      * 异步请求
-     *
-     * @param callback
-     * @return
      */
-    RequestHandler execute(RequestCallback callback);
+    fun execute(callback: RequestCallback?): RequestHandler
 
     /**
      * 异步请求，在单线程线程池执行(发起的异步请求会按顺序一个个执行)
-     *
-     * @param callback
-     * @return
      */
-    RequestHandler executeSequence(RequestCallback callback);
+    fun executeSequence(callback: RequestCallback?): RequestHandler
 
     /**
      * 同步请求
      */
-    IResponse execute() throws Exception;
+    @Throws(Exception::class)
+    fun execute(): IResponse
+
+    companion object {
+        /**默认连接超时*/
+        const val DEFAULT_CONNECT_TIMEOUT = 15 * 1000
+
+        /**默认读取超时*/
+        const val DEFAULT_READ_TIMEOUT = 15 * 1000
+    }
 }
--- lib/src/main/java/com/sd/lib/http/Request.kt
+++ lib/src/main/java/com/sd/lib/http/Request.kt
@@ -1,229 +1,94 @@
-package com.sd.lib.http;
+package com.sd.lib.http
 
-import com.sd.lib.http.callback.IUploadProgressCallback;
-import com.sd.lib.http.callback.RequestCallback;
-import com.sd.lib.http.utils.HttpDataHolder;
-import com.sd.lib.http.utils.TransmitParam;
+import com.sd.lib.http.callback.IUploadProgressCallback
+import com.sd.lib.http.callback.RequestCallback
+import com.sd.lib.http.utils.HttpDataHolder
+import com.sd.lib.http.utils.TransmitParam
+import javax.net.ssl.HostnameVerifier
+import javax.net.ssl.SSLSocketFactory
 
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.SSLSocketFactory;
+abstract class Request : IRequest {
 
-public abstract class Request implements IRequest
-{
-    private String mBaseUrl;
-    private String mUrlSuffix;
-
-    private final HttpDataHolder<String, Object> mParams = new HttpDataHolder<>();
-    private final HttpDataHolder<String, String> mHeaders = new HttpDataHolder<>();
-
-    private String mTag;
-
-    private int mConnectTimeout = DEFAULT_CONNECT_TIMEOUT;
-    private int mReadTimeout = DEFAULT_READ_TIMEOUT;
-
-    private SSLSocketFactory mSSLSocketFactory;
-    private HostnameVerifier mHostnameVerifier;
-
-    private IUploadProgressCallback mUploadProgressCallback;
-    private TransmitParam mTransmitParam;
-
-    private boolean mInterceptExecute = true;
+    private val uploadTransmitParam: TransmitParam by lazy {
+        TransmitParam()
+    }
 
     //---------- IRequest implements start ----------
 
-    @Override
-    public final void setBaseUrl(String baseUrl)
-    {
-        mBaseUrl = baseUrl;
+    override var baseUrl: String = ""
+
+    override var urlSuffix: String = ""
+
+    override val url: String
+        get() = baseUrl + urlSuffix
+
+    override var tag: String? = null
+
+    override var connectTimeout = IRequest.DEFAULT_CONNECT_TIMEOUT
+
+    override var readTimeout = IRequest.DEFAULT_READ_TIMEOUT
+
+    override val params = HttpDataHolder<String, Any>()
+
+    override val headers = HttpDataHolder<String, String>()
+
+    override var interceptExecute: Boolean = false
+
+    override var uploadProgressCallback: IUploadProgressCallback? = null
+
+    override var sSLSocketFactory: SSLSocketFactory? = null
+
+    override var hostnameVerifier: HostnameVerifier? = null
+
+    override fun execute(callback: RequestCallback?): RequestHandler {
+        return RequestManager.getInstance().execute(this, callback)
     }
 
-    @Override
-    public final void setUrlSuffix(String urlSuffix)
-    {
-        mUrlSuffix = urlSuffix;
+    override fun executeSequence(callback: RequestCallback?): RequestHandler {
+        return RequestManager.getInstance().execute(this, true, callback)
     }
 
-    @Override
-    public final void setTag(String tag)
-    {
-        mTag = tag;
-    }
-
-    @Override
-    public final void setConnectTimeout(int connectTimeout)
-    {
-        mConnectTimeout = connectTimeout;
-    }
-
-    @Override
-    public final void setReadTimeout(int readTimeout)
-    {
-        mReadTimeout = readTimeout;
-    }
-
-    @Override
-    public final void setUploadProgressCallback(IUploadProgressCallback callback)
-    {
-        mUploadProgressCallback = callback;
-    }
-
-    @Override
-    public final void setSSLSocketFactory(SSLSocketFactory sslSocketFactory)
-    {
-        mSSLSocketFactory = sslSocketFactory;
-    }
-
-    public final void setHostnameVerifier(HostnameVerifier hostnameVerifier)
-    {
-        mHostnameVerifier = hostnameVerifier;
-    }
-
-    @Override
-    public final void setInterceptExecute(boolean interceptExecute)
-    {
-        mInterceptExecute = interceptExecute;
-    }
-
-    @Override
-    public final HttpDataHolder<String, Object> getParams()
-    {
-        return mParams;
-    }
-
-    @Override
-    public final HttpDataHolder<String, String> getHeaders()
-    {
-        return mHeaders;
-    }
-
-    @Override
-    public final String getBaseUrl()
-    {
-        if (mBaseUrl == null)
-            mBaseUrl = "";
-        return mBaseUrl;
-    }
-
-    @Override
-    public final String getUrlSuffix()
-    {
-        if (mUrlSuffix == null)
-            mUrlSuffix = "";
-        return mUrlSuffix;
-    }
-
-    @Override
-    public String getUrl()
-    {
-        return getBaseUrl() + getUrlSuffix();
-    }
-
-    @Override
-    public final String getTag()
-    {
-        return mTag;
-    }
-
-    @Override
-    public final SSLSocketFactory getSSLSocketFactory()
-    {
-        return mSSLSocketFactory;
-    }
-
-    @Override
-    public final HostnameVerifier getHostnameVerifier()
-    {
-        return mHostnameVerifier;
-    }
-
-    @Override
-    public final RequestHandler execute(RequestCallback callback)
-    {
-        return RequestManager.getInstance().execute(this, callback);
-    }
-
-    @Override
-    public final RequestHandler executeSequence(RequestCallback callback)
-    {
-        return RequestManager.getInstance().execute(this, true, callback);
-    }
-
-    @Override
-    public final IResponse execute() throws Exception
-    {
-        final boolean intercept = mInterceptExecute;
-
-        if (intercept)
-        {
-            try
-            {
-                final IResponse beforeResponse = RequestManager.getInstance().mInternalRequestInterceptor.beforeExecute(this);
-                if (beforeResponse != null)
-                    return beforeResponse;
-            } catch (Exception e)
-            {
-                RequestManager.getInstance().mInternalRequestInterceptor.onError(e);
+    @Throws(Exception::class)
+    override fun execute(): IResponse {
+        val intercept = interceptExecute
+        if (intercept) {
+            try {
+                val beforeResponse = RequestManager.getInstance().mInternalRequestInterceptor.beforeExecute(this)
+                if (beforeResponse != null) return beforeResponse
+            } catch (e: Exception) {
+                RequestManager.getInstance().mInternalRequestInterceptor.onError(e)
             }
         }
 
-        final IResponse realResponse = doExecute();
-
-        if (intercept)
-        {
-            try
-            {
-                final IResponse afterResponse = RequestManager.getInstance().mInternalRequestInterceptor.afterExecute(this, realResponse);
-                if (afterResponse != null)
-                    return afterResponse;
-            } catch (Exception e)
-            {
-                RequestManager.getInstance().mInternalRequestInterceptor.onError(e);
+        val realResponse = doExecute()
+        if (intercept) {
+            try {
+                val afterResponse = RequestManager.getInstance().mInternalRequestInterceptor.afterExecute(this, realResponse)
+                if (afterResponse != null) return afterResponse
+            } catch (e: Exception) {
+                RequestManager.getInstance().mInternalRequestInterceptor.onError(e)
             }
         }
-
-        return realResponse;
+        return realResponse
     }
-
     //---------- IRequest implements end ----------
-
     /**
      * 发起请求
      *
      * @return
      * @throws Exception
      */
-    protected abstract IResponse doExecute() throws Exception;
+    @Throws(Exception::class)
+    protected abstract fun doExecute(): IResponse
 
-    protected final int getReadTimeout()
-    {
-        return mReadTimeout;
-    }
+    protected fun notifyProgressUpload(uploaded: Long, total: Long) {
+        val callback = uploadProgressCallback ?: return
 
-    protected final int getConnectTimeout()
-    {
-        return mConnectTimeout;
-    }
-
-    protected final void notifyProgressUpload(long uploaded, long total)
-    {
-        if (mUploadProgressCallback == null)
-            return;
-
-        if (mTransmitParam == null)
-            mTransmitParam = new TransmitParam();
-
-        if (mTransmitParam.transmit(total, uploaded))
-        {
-            final TransmitParam param = mTransmitParam.copy();
-            FTask.runOnUiThread(new Runnable()
-            {
-                @Override
-                public void run()
-                {
-                    if (mUploadProgressCallback != null)
-                        mUploadProgressCallback.onProgressUpload(param);
-                }
-            });
+        if (uploadTransmitParam.transmit(total, uploaded)) {
+            val param = uploadTransmitParam.copy()
+            FTask.runOnUiThread {
+                callback.onProgressUpload(param)
+            }
         }
     }
 }
