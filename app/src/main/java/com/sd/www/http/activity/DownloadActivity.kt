019b861a8628ee32f53c66418eb40db067bd28ca
zjun <565061763@qq.com>
2021-03-06 17:02:48 +0800

    demo

--- app/src/main/java/com/sd/www/http/activity/DownloadActivity.kt
+++ app/src/main/java/com/sd/www/http/activity/DownloadActivity.kt
@@ -1,89 +1,73 @@
-package com.sd.www.http.activity;
+package com.sd.www.http.activity
 
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.ProgressBar;
-import android.widget.TextView;
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.sd.lib.http.Request
+import com.sd.lib.http.RequestManager
+import com.sd.lib.http.callback.FileRequestCallback
+import com.sd.lib.http.impl.GetRequest
+import com.sd.lib.http.utils.TransmitParam
+import com.sd.www.http.databinding.ActivityDownloadBinding
+import java.io.File
 
-import androidx.appcompat.app.AppCompatActivity;
+class DownloadActivity : AppCompatActivity(), View.OnClickListener {
+    private val TAG = DownloadActivity::class.java.simpleName
+    private val URL_FILE = "https://qd.myapp.com/myapp/qqteam/AndroidQQ/mobileqq_android.apk"
 
-import com.sd.lib.http.Request;
-import com.sd.lib.http.RequestManager;
-import com.sd.lib.http.callback.FileRequestCallback;
-import com.sd.lib.http.impl.GetRequest;
-import com.sd.lib.http.utils.TransmitParam;
-import com.sd.www.http.R;
+    private lateinit var mBinding: ActivityDownloadBinding
 
-import java.io.File;
-
-public class DownloadActivity extends AppCompatActivity
-{
-    public static final String TAG = DownloadActivity.class.getSimpleName();
-    public static final String URL_FILE = "https://qd.myapp.com/myapp/qqteam/AndroidQQ/mobileqq_android.apk";
-
-    private ProgressBar mProgressBar;
-    private TextView mTvProgress, mTvSpeed;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_download);
-        mProgressBar = findViewById(R.id.progressBar);
-        mTvProgress = findViewById(R.id.tv_progress);
-        mTvSpeed = findViewById(R.id.tv_speed);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mBinding = ActivityDownloadBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
     }
 
-    public void onClickRequest(View view)
-    {
-        final File file = new File(getExternalCacheDir(), "download.apk");
-
-        final Request request = new GetRequest();
-        request.setBaseUrl(URL_FILE);
-        request.setTag(TAG);
-        request.execute(new FileRequestCallback(file)
-        {
-            @Override
-            protected void onProgressDownload(TransmitParam param)
-            {
-                mProgressBar.setProgress(param.getProgress());
-                mTvProgress.setText(param.getProgress() + "%");
-                mTvSpeed.setText(param.getSpeedKBps() + "KB/秒");
-            }
-
-            @Override
-            public void onSuccess()
-            {
-                Log.i(TAG, "download finish");
-            }
-
-            @Override
-            public void onError(Exception e)
-            {
-                super.onError(e);
-                Log.i(TAG, "download error:" + e);
-            }
-
-            @Override
-            public void onCancel()
-            {
-                super.onCancel();
-                mTvSpeed.setText("");
-                Log.i(TAG, "download cancelled");
-            }
-        });
+    override fun onClick(v: View) {
+        if (v === mBinding.btnStart) {
+            startDownload()
+        } else if (v === mBinding.btnStop) {
+            stopDownload()
+        }
     }
 
-    public void onClickCancelRequest(View view)
-    {
-        RequestManager.getInstance().cancelTag(TAG);
+    private fun startDownload() {
+        val file = File(externalCacheDir, "download.apk")
+
+        val request: Request = GetRequest()
+        request.baseUrl = URL_FILE
+        request.tag = TAG
+        request.execute(object : FileRequestCallback(file) {
+            override fun onProgressDownload(param: TransmitParam) {
+                mBinding.progressBar.progress = param.progress
+                mBinding.tvProgress.text = "${param.progress}%"
+                mBinding.tvSpeed.text = "${param.speedKBps}KB/秒"
+            }
+
+            override fun onSuccess() {
+                Log.i(TAG, "download finish")
+            }
+
+            override fun onError(e: Exception) {
+                super.onError(e)
+                Log.i(TAG, "download error:$e")
+            }
+
+            override fun onCancel() {
+                super.onCancel()
+                mBinding.tvSpeed.text = ""
+                Log.i(TAG, "download cancelled")
+            }
+        })
     }
 
-    @Override
-    protected void onDestroy()
-    {
-        super.onDestroy();
-        RequestManager.getInstance().cancelTag(TAG);
+    private fun stopDownload() {
+        RequestManager.getInstance().cancelTag(TAG)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        stopDownload()
     }
 }
