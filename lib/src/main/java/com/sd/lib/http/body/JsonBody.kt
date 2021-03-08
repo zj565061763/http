c8419d22d627d77b084b8e832caceef2d2c5a47f
zjun <565061763@qq.com>
2021-03-08 18:06:55 +0800

    JsonBody

--- lib/src/main/java/com/sd/lib/http/body/JsonBody.kt
+++ lib/src/main/java/com/sd/lib/http/body/JsonBody.kt
@@ -2,6 +2,5 @@ package com.sd.lib.http.body
 
 import com.sd.lib.http.ContentType
 
-class JsonBody : StringBody {
-    constructor(body: String?) : super(body, ContentType.JSON)
+class JsonBody(body: String?) : StringBody(body, ContentType.JSON) {
 }
