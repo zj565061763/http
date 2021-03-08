00d2a99417013806bdf17d059cf05a839f36ce6d
zjun <565061763@qq.com>
2021-03-08 15:39:00 +0800

    demo

--- app/src/main/java/com/sd/www/http/activity/AsyncRequestActivity.kt
+++ app/src/main/java/com/sd/www/http/activity/AsyncRequestActivity.kt
@@ -52,67 +52,73 @@ class AsyncRequestActivity : AppCompatActivity(), View.OnClickListener {
         request.tag = TAG
 
         // 发起异步请求
-        val requestHandler = request.execute(object : ModelRequestCallback<WeatherModel?>() {
-            override fun onPrepare(request: IRequest) {
-                super.onPrepare(request)
-                // 请求在执行之前的准备回调（发起请求的线程）
-                Log.i(TAG, "onPrepare----->")
-            }
-
-            override fun onStart() {
-                super.onStart()
-                // 开始回调（UI线程）
-                Log.i(TAG, "onStart")
-            }
-
-            @Throws(Exception::class)
-            override fun onSuccessBackground() {
-                super.onSuccessBackground()
-                // 成功回调，super里面回调了parseToModel方法把返回的内容转为实体（非UI线程）
-                Log.i(TAG, "onSuccessBackground")
-            }
-
-            override fun parseToModel(content: String, type: Type): WeatherModel {
-                // 把返回的内容转实体（非UI线程）
-                return Gson().fromJson(content, type)
-            }
-
-            override fun onSuccessBefore() {
-                // 成功回调（UI线程）
-                Log.i(TAG, "onSuccessBefore")
-            }
-
-            override fun onSuccess() {
-                // 成功回调（UI线程）
-
-                // 获得返回结果对象
-                val response = response
-                // 获得接口对应的实体
-                val model = actModel
-                Log.i(TAG, "onSuccess:" + model!!.weatherinfo!!.city)
-            }
-
-            override fun onError(e: Exception) {
-                super.onError(e)
-                // 异常回调，请求异常或者成功之后的数据处理异常（UI线程）
-                Log.i(TAG, "onError:$e")
-            }
-
-            override fun onCancel() {
-                super.onCancel()
-                // 取消回调（UI线程）
-                Log.i(TAG, "onCancel")
-            }
-
-            override fun onFinish() {
-                super.onFinish()
-                // 结束回调（UI线程）
-                Log.i(TAG, "onFinish")
-            }
-        })
+        val requestHandler = request.execute(mModelRequestCallback)
 
         // 保存请求处理对象，用于取消
         mRequestHandlers[requestHandler] = ""
+    }
+
+    /**
+     * 回调对象
+     */
+    private val mModelRequestCallback = object : ModelRequestCallback<WeatherModel>() {
+
+        override fun onPrepare(request: IRequest) {
+            super.onPrepare(request)
+            // 请求在执行之前的准备回调（发起请求的线程）
+            Log.i(TAG, "onPrepare----->")
+        }
+
+        override fun onStart() {
+            super.onStart()
+            // 开始回调（UI线程）
+            Log.i(TAG, "onStart")
+        }
+
+        @Throws(Exception::class)
+        override fun onSuccessBackground() {
+            super.onSuccessBackground()
+            // 成功回调，super里面回调了parseToModel方法把返回的内容转为实体（非UI线程）
+            Log.i(TAG, "onSuccessBackground")
+        }
+
+        override fun parseToModel(content: String, type: Type): WeatherModel {
+            // 把返回的内容转实体（非UI线程）
+            return Gson().fromJson(content, type)
+        }
+
+        override fun onSuccessBefore() {
+            // 成功回调（UI线程）
+            Log.i(TAG, "onSuccessBefore")
+        }
+
+        override fun onSuccess() {
+            // 成功回调（UI线程）
+
+            // 获得返回结果对象
+            val response = response!!
+            // 获得接口对应的实体
+            val model = actModel!!
+            Log.i(TAG, "onSuccess:" + model.weatherinfo!!.city)
+        }
+
+        override fun onError(e: Exception) {
+            super.onError(e)
+            // 异常回调，请求异常或者成功之后的数据处理异常（UI线程）
+            Log.i(TAG, "onError:$e")
+        }
+
+        override fun onCancel() {
+            super.onCancel()
+            // 取消回调（UI线程）
+            Log.i(TAG, "onCancel")
+        }
+
+        override fun onFinish() {
+            super.onFinish()
+            // 结束回调（UI线程）
+            Log.i(TAG, "onFinish")
+        }
     }
 
     /**
