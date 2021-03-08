b931a1e406daa5c5b380dec4934442c505c4e928
zjun <565061763@qq.com>
2021-03-08 10:22:15 +0800

    kotlin RequestCallback

--- lib/src/main/java/com/sd/lib/http/callback/RequestCallback.kt
+++ lib/src/main/java/com/sd/lib/http/callback/RequestCallback.kt
@@ -1,85 +1,72 @@
-package com.sd.lib.http.callback;
+package com.sd.lib.http.callback
 
-import com.sd.lib.http.IRequest;
-import com.sd.lib.http.IResponse;
-import com.sd.lib.http.exception.HttpExceptionResponseCode;
-import com.sd.lib.http.utils.TransmitParam;
+import com.sd.lib.http.IRequest
+import com.sd.lib.http.IResponse
+import com.sd.lib.http.exception.HttpExceptionResponseCode.Companion.from
+import com.sd.lib.http.utils.TransmitParam
 
-public abstract class RequestCallback implements IUploadProgressCallback
-{
-    private IRequest mRequest;
-    private IResponse mResponse;
-
-    public void setRequest(IRequest request)
-    {
-        this.mRequest = request;
-    }
-
-    public IRequest getRequest()
-    {
-        return mRequest;
-    }
-
-    public void setResponse(IResponse response)
-    {
-        mResponse = response;
-    }
-
-    public IResponse getResponse()
-    {
-        return mResponse;
-    }
+abstract class RequestCallback : IUploadProgressCallback {
+    open var request: IRequest? = null
+    open var response: IResponse? = null
 
     //---------- notify method start ----------
 
-    public void onPrepare(IRequest request)
-    {
-    }
+    /**
+     * 预备回调（和提交请求对象在同一个线程）
+     */
+    open fun onPrepare(request: IRequest) {}
 
-    public void onStart()
-    {
-    }
+    /**
+     * 开始回调（UI线程）
+     */
+    open fun onStart() {}
 
-    public void onSuccessBackground() throws Exception
-    {
-        processResponseCode(getResponse().getCode());
+    /**
+     * 成功回调，常用来处理解析数据（后台线程）
+     */
+    @Throws(Exception::class)
+    open fun onSuccessBackground() {
+        processResponseCode(response!!.code)
     }
 
     /**
      * 处理返回码
-     *
-     * @param code
-     * @throws Exception
      */
-    protected void processResponseCode(int code) throws Exception
-    {
-        final HttpExceptionResponseCode codeException = HttpExceptionResponseCode.from(code);
-        if (codeException != null)
-            throw codeException;
+    @Throws(Exception::class)
+    protected fun processResponseCode(code: Int) {
+        val codeException = from(code)
+        if (codeException != null) throw codeException
     }
 
-    public void onSuccessBefore()
-    {
-    }
+    /**
+     * 成功回调，常用来做一些统一的处理（UI线程）
+     */
+    open fun onSuccessBefore() {}
 
-    public abstract void onSuccess();
+    /**
+     * 成功回调，在[onSuccessBefore]方法之后执行（UI线程）
+     */
+    abstract fun onSuccess()
 
-    @Override
-    public void onProgressUpload(TransmitParam param)
-    {
-    }
+    /**
+     * 上传回调（UI线程）
+     */
+    override fun onProgressUpload(param: TransmitParam) {}
 
-    public void onError(Exception e)
-    {
-    }
+    /**
+     * 错误回调（UI线程）
+     */
+    open fun onError(e: Exception) {}
 
-    public void onCancel()
-    {
-    }
+    /**
+     * 取消回调（UI线程）
+     */
+    open fun onCancel() {}
 
-    public void onFinish()
-    {
-    }
+    /**
+     * 结束回调（UI线程）
+     */
+    open fun onFinish() {}
 
     //---------- notify method end ----------
 }
