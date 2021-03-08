df8b0f962980e2a2d238c492e962332e2a6ab278
zjun <565061763@qq.com>
2021-03-08 18:01:50 +0800

    FileBody

--- lib/src/main/java/com/sd/lib/http/body/FileBody.kt
+++ lib/src/main/java/com/sd/lib/http/body/FileBody.kt
@@ -5,20 +5,15 @@ import com.sd.lib.http.ContentType
 import java.io.File
 import java.net.HttpURLConnection
 
-class FileBody : IRequestBody<File> {
+class FileBody(file: File) : IRequestBody<File> {
 
-    constructor(file: File) {
-        this.body = file
-    }
+    override val body: File = file
 
-    override val body: File
-
-    override val contentType: String
-        get() {
-            var contentType = HttpURLConnection.guessContentTypeFromName(body.name)
-            if (TextUtils.isEmpty(contentType)) {
-                contentType = ContentType.STREAM
-            }
-            return contentType
+    override val contentType: String by lazy {
+        var contentType = HttpURLConnection.guessContentTypeFromName(body.name)
+        if (TextUtils.isEmpty(contentType)) {
+            contentType = ContentType.STREAM
         }
+        contentType
+    }
 }
