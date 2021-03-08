0622d2105266b5e3bb8232885c911eb790e73394
zjun <565061763@qq.com>
2021-03-08 16:31:09 +0800

    kotlin FileRequestCallback

--- lib/src/main/java/com/sd/lib/http/callback/FileRequestCallback.kt
+++ lib/src/main/java/com/sd/lib/http/callback/FileRequestCallback.kt
@@ -1,87 +1,51 @@
-package com.sd.lib.http.callback;
+package com.sd.lib.http.callback
 
-import com.sd.lib.http.IResponse;
-import com.sd.lib.http.utils.HttpIOUtils;
-import com.sd.lib.http.utils.HttpUtils;
-import com.sd.lib.http.utils.TransmitParam;
+import com.sd.lib.http.utils.HttpIOUtils.Companion.closeQuietly
+import com.sd.lib.http.utils.HttpIOUtils.Companion.copy
+import com.sd.lib.http.utils.HttpUtils.Companion.removeCallbacks
+import com.sd.lib.http.utils.HttpUtils.Companion.runOnUiThread
+import com.sd.lib.http.utils.TransmitParam
+import java.io.File
+import java.io.FileOutputStream
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
+abstract class FileRequestCallback : RequestCallback {
+    val file: File
+    val transmitParam = TransmitParam()
 
-public abstract class FileRequestCallback extends RequestCallback
-{
-    private final File mFile;
-    private final TransmitParam mTransmitParam = new TransmitParam();
-
-    public FileRequestCallback(File file)
-    {
-        if (file == null)
-            throw new IllegalArgumentException("file is null");
-        mFile = file;
+    constructor(file: File) {
+        this.file = file
     }
 
-    public final File getFile()
-    {
-        return mFile;
-    }
-
-    public final TransmitParam getTransmitParam()
-    {
-        return mTransmitParam;
-    }
-
-    @Override
-    public void onSuccessBackground() throws Exception
-    {
-        super.onSuccessBackground();
-
-        final IResponse response = getResponse();
-        final long total = response.getContentLength();
-        final InputStream input = response.getInputStream();
-        final OutputStream output = new FileOutputStream(getFile());
-        try
-        {
-            HttpIOUtils.copy(input, output, new HttpIOUtils.ProgressCallback()
-            {
-                @Override
-                public boolean onProgress(long count)
-                {
-                    if (!getRequestTaskApi().isActive())
-                    {
-                        // 任务已经死亡，停止下载
-                        return true;
-                    }
-
-                    if (getTransmitParam().transmit(total, count))
-                        HttpUtils.runOnUiThread(mUpdateProgressRunnable);
-                    return false;
+    @Throws(Exception::class)
+    override fun onSuccessBackground() {
+        super.onSuccessBackground()
+        val total = response!!.contentLength.toLong()
+        val input = response!!.inputStream
+        val output = FileOutputStream(file)
+        try {
+            copy(input, output) { count ->
+                if (!requestTaskApi!!.isActive) {
+                    // 任务已经死亡，停止下载
+                    true
                 }
-            });
-        } finally
-        {
-            HttpUtils.runOnUiThread(mUpdateProgressRunnable);
-            HttpIOUtils.closeQuietly(input);
-            HttpIOUtils.closeQuietly(output);
+                if (transmitParam.transmit(total, count)) {
+                    runOnUiThread(mUpdateProgressRunnable)
+                }
+                false
+            }
+        } finally {
+            runOnUiThread(mUpdateProgressRunnable)
+            closeQuietly(input)
+            closeQuietly(output)
         }
     }
 
-    private final Runnable mUpdateProgressRunnable = new Runnable()
-    {
-        @Override
-        public void run()
-        {
-            onProgressDownload(getTransmitParam());
-        }
-    };
+    private val mUpdateProgressRunnable = Runnable { onProgressDownload(transmitParam) }
 
-    protected abstract void onProgressDownload(TransmitParam param);
+    protected abstract fun onProgressDownload(param: TransmitParam)
 
-    @Override
-    public void onCancel()
-    {
-        super.onCancel();
-        HttpUtils.removeCallbacks(mUpdateProgressRunnable);
+    override fun onCancel() {
+        super.onCancel()
+        removeCallbacks(mUpdateProgressRunnable)
     }
 }
