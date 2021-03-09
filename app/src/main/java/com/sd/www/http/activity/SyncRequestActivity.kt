282c01e3f8d272f698a0df8b38f8ba7cca0389c6
zjun <565061763@qq.com>
2021-03-09 18:10:49 +0800

    demo

--- app/src/main/java/com/sd/www/http/activity/SyncRequestActivity.kt
+++ app/src/main/java/com/sd/www/http/activity/SyncRequestActivity.kt
@@ -3,15 +3,17 @@ package com.sd.www.http.activity
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.sd.lib.http.IRequest
 import com.sd.lib.http.impl.GetRequest
 import com.sd.www.http.databinding.ActivitySyncRequestBinding
+import kotlinx.coroutines.*
 
 /**
  * 同步请求demo
  */
 class SyncRequestActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var mBinding: ActivitySyncRequestBinding
+
+    val mMainScope: CoroutineScope = MainScope()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,20 +28,27 @@ class SyncRequestActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     private fun requestData() {
-        Thread {
-            try {
-                val request: IRequest = GetRequest()
-                //设置请求地址
-                request.baseUrl = "https://www.baidu.com/"
-                //发起请求，得到Response对象
-                val response = request.execute()
-                //请求结果以字符串返回
-                val result = response.asString
+        mMainScope.launch {
+            val content = getContent()
+            mBinding.tvResult.text = content
+        }
+    }
 
-                runOnUiThread { mBinding.tvResult.text = result }
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-        }.start()
+    private suspend fun getContent(): String {
+        return withContext(Dispatchers.IO) {
+            // 创建请求对象
+            val request = GetRequest()
+            // 设置请求地址
+            request.baseUrl = "https://www.baidu.com/"
+            // 发起请求，得到Response对象
+            val response = request.execute()
+            // 请求结果以字符串返回
+            response.asString
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mMainScope.cancel()
     }
 }
