f8f6568d46cc8d9012a51025e0e4b7ff60ac9307
zjun <565061763@qq.com>
2021-03-10 09:14:10 +0800

    RequestHandler

--- lib/src/main/java/com/sd/lib/http/RequestHandler.kt
+++ lib/src/main/java/com/sd/lib/http/RequestHandler.kt
@@ -3,12 +3,8 @@ package com.sd.lib.http
 /**
  * 执行异步请求后返回的对象，提供一些方法暴露给外部
  */
-class RequestHandler {
-    private val requestTask: RequestTask
-
-    internal constructor(task: RequestTask) {
-        this.requestTask = task
-    }
+class RequestHandler internal constructor(task: RequestTask) {
+    private val requestTask: RequestTask = task
 
     /**
      * 取消请求
