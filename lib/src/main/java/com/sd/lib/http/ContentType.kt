0f94aecd6c822824938e72f25e0393606a244245
zjun <565061763@qq.com>
2021-05-15 01:19:07 +0800

    ContentType

--- lib/src/main/java/com/sd/lib/http/ContentType.kt
+++ lib/src/main/java/com/sd/lib/http/ContentType.kt
@@ -1,10 +1,8 @@
 package com.sd.lib.http
 
-class ContentType private constructor() {
-    companion object {
-        const val STREAM = "application/octet-stream"
-        const val JSON = "application/json"
-        const val FORM = "application/x-www-form-urlencoded"
-        const val FORM_MULTIPART = "multipart/form-data"
-    }
+object ContentType {
+    const val STREAM = "application/octet-stream"
+    const val JSON = "application/json"
+    const val FORM = "application/x-www-form-urlencoded"
+    const val FORM_MULTIPART = "multipart/form-data"
 }
