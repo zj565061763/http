fd0c144d13a9733b54f63cb1fcd9265ba3f81c23
zjun <565061763@qq.com>
2021-05-15 01:21:23 +0800

    HttpLog

--- lib/src/main/java/com/sd/lib/http/utils/HttpLog.kt
+++ lib/src/main/java/com/sd/lib/http/utils/HttpLog.kt
@@ -3,23 +3,20 @@ package com.sd.lib.http.utils
 import android.util.Log
 import com.sd.lib.http.RequestManager
 
-internal class HttpLog private constructor() {
+internal object HttpLog {
+    private const val TAG = "FHttp"
 
-    companion object {
-        private const val TAG = "FHttp"
-
-        @JvmStatic
-        fun i(msg: String?) {
-            if (RequestManager.instance.isDebug) {
-                Log.i(TAG, msg)
-            }
+    @JvmStatic
+    fun i(msg: String) {
+        if (RequestManager.instance.isDebug) {
+            Log.i(TAG, msg)
         }
+    }
 
-        @JvmStatic
-        fun e(msg: String?) {
-            if (RequestManager.instance.isDebug) {
-                Log.e(TAG, msg)
-            }
+    @JvmStatic
+    fun e(msg: String) {
+        if (RequestManager.instance.isDebug) {
+            Log.e(TAG, msg)
         }
     }
 }
