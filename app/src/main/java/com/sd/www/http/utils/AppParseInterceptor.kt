90e503f0e5a53197efde580c8b800693602c3447
zjun <565061763@qq.com>
2021-05-16 00:20:17 +0800

    demo

--- app/src/main/java/com/sd/www/http/utils/AppParseInterceptor.kt
+++ app/src/main/java/com/sd/www/http/utils/AppParseInterceptor.kt
@@ -2,9 +2,15 @@ package com.sd.www.http.utils
 
 import com.sd.lib.http.interceptor.IParseInterceptor
 import com.sd.lib.result.FResult
+import com.sd.lib.utils.context.FToast
 
 class AppParseInterceptor : IParseInterceptor {
     override fun intercept(result: FResult<*>): Boolean {
-        TODO("Not yet implemented")
+        if (result.isFailure) {
+            FToast.show("解析拦截器：${result.failure}")
+        }
+
+        // 返回true拦截，false不拦截
+        return false
     }
 }
