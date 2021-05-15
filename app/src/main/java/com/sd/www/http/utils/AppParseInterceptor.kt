f8338ee6c2d349b3ef166b1d22b6e9eaffcb1b47
zjun <565061763@qq.com>
2021-05-16 00:21:43 +0800

    demo

--- app/src/main/java/com/sd/www/http/utils/AppParseInterceptor.kt
+++ app/src/main/java/com/sd/www/http/utils/AppParseInterceptor.kt
@@ -8,6 +8,7 @@ class AppParseInterceptor : IParseInterceptor {
     override fun intercept(result: FResult<*>): Boolean {
         if (result.isFailure) {
             FToast.show("解析拦截器：${result.failure}")
+            // return true
         }
 
         // 返回true拦截，false不拦截
