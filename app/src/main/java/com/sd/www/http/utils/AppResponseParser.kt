02e28e83f13750a5a521d588eda216b6ee13f526
zjun <565061763@qq.com>
2021-05-17 22:13:17 +0800

    demo

--- app/src/main/java/com/sd/www/http/utils/AppResponseParser.kt
+++ app/src/main/java/com/sd/www/http/utils/AppResponseParser.kt
@@ -1,10 +1,14 @@
 package com.sd.www.http.utils
 
+import android.util.Log
 import com.sd.lib.http.IRequest
 import com.sd.lib.http.parser.DefaultResponseParser
 
 class AppResponseParser : DefaultResponseParser() {
+    val TAG = AppResponseParser::class.java.simpleName
+
     override fun <T> parse(content: String, clazz: Class<T>, request: IRequest): T {
+        Log.i(TAG, "parse content:${request} clazz:${clazz} request:${request}")
         return super.parse(content, clazz, request)
     }
 }
