1c1e502d3c2582c0ad58857354bae35f3e688c2f
zjun <565061763@qq.com>
2021-05-17 17:24:27 +0800

    getDescFormat

--- lib/src/main/java/com/sd/lib/http/exception/HttpException.kt
+++ lib/src/main/java/com/sd/lib/http/exception/HttpException.kt
@@ -1,10 +1,19 @@
 package com.sd.lib.http.exception
 
+import android.content.Context
+import com.sd.lib.http.R
 import com.sd.lib.result.exception.FExceptionHttp
 
 open class HttpException : FExceptionHttp {
     @JvmOverloads
     constructor(message: String? = "", cause: Throwable? = null) : super(message, cause)
+
+    /**
+     * 返回格式化好的描述文字
+     */
+    open fun getDescFormat(context: Context): String {
+        return context.getString(R.string.lib_http_desc_exception_http)
+    }
 
     companion object {
         @JvmStatic
