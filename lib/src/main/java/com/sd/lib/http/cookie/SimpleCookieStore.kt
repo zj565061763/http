db025ed8860a2093f15a956456c132062907d3fd
zjun <565061763@qq.com>
2021-05-17 09:26:37 +0800

    SimpleCookieStore

---
+++ lib/src/main/java/com/sd/lib/http/cookie/SimpleCookieStore.kt
@@ -0,0 +1,38 @@
+package com.sd.lib.http.cookie
+
+import java.net.HttpCookie
+import java.net.URI
+
+open class SimpleCookieStore : ICookieStore {
+    private val _cookieStore by lazy { InMemoryCookieStore() }
+
+    override fun add(uri: URI?, listCookie: List<HttpCookie?>?) {
+        listCookie?.forEach {
+            add(uri, it)
+        }
+    }
+
+    override fun add(uri: URI?, cookie: HttpCookie?) {
+        _cookieStore.add(uri, cookie)
+    }
+
+    override fun get(uri: URI?): MutableList<HttpCookie> {
+        return _cookieStore.get(uri)
+    }
+
+    override fun getCookies(): MutableList<HttpCookie> {
+        return _cookieStore.cookies
+    }
+
+    override fun getURIs(): MutableList<URI> {
+        return _cookieStore.urIs
+    }
+
+    override fun remove(uri: URI?, cookie: HttpCookie?): Boolean {
+        return _cookieStore.remove(uri, cookie)
+    }
+
+    override fun removeAll(): Boolean {
+        return _cookieStore.removeAll()
+    }
+}
