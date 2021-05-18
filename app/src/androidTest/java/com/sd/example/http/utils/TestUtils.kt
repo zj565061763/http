28a14db1f5ce4546d9dbeb41d5eed67693543763
zjun <565061763@qq.com>
2021-05-18 22:56:33 +0800

    test

--- app/src/androidTest/java/com/sd/example/http/utils/TestUtils.kt
+++ app/src/androidTest/java/com/sd/example/http/utils/TestUtils.kt
@@ -17,55 +17,47 @@ open class LifecycleRequestCallback : RequestCallback() {
     @CallSuper
     override fun onPrepare(request: IRequest) {
         super.onPrepare(request)
-        assert(!list.contains(Lifecycle.onPrepare))
         list.add(Lifecycle.onPrepare)
     }
 
     @CallSuper
     override fun onStart() {
         super.onStart()
-        assert(!list.contains(Lifecycle.onStart))
         list.add(Lifecycle.onStart)
     }
 
     @CallSuper
     override fun onResponseBackground(response: IResponse) {
         super.onResponseBackground(response)
-        assert(!list.contains(Lifecycle.onResponseBackground))
         list.add(Lifecycle.onResponseBackground)
     }
 
     @CallSuper
     override fun onSuccessBefore() {
         super.onSuccessBefore()
-        assert(!list.contains(Lifecycle.onSuccessBefore))
         list.add(Lifecycle.onSuccessBefore)
     }
 
     @CallSuper
     override fun onSuccess() {
-        assert(!list.contains(Lifecycle.onSuccess))
         list.add(Lifecycle.onSuccess)
     }
 
     @CallSuper
     override fun onError(e: Exception) {
         super.onError(e)
-        assert(!list.contains(Lifecycle.onError))
         list.add(Lifecycle.onError)
     }
 
     @CallSuper
     override fun onCancel() {
         super.onCancel()
-        assert(!list.contains(Lifecycle.onCancel))
         list.add(Lifecycle.onCancel)
     }
 
     @CallSuper
     override fun onFinish() {
         super.onFinish()
-        assert(!list.contains(Lifecycle.onFinish))
         list.add(Lifecycle.onFinish)
     }
 }
