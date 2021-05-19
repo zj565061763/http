374ee732ffdc879f9dae64d5c1e4f71b191823e6
zjun <565061763@qq.com>
2021-05-19 20:56:57 +0800

    StringBody

--- lib/src/main/java/com/sd/lib/http/body/StringBody.kt
+++ lib/src/main/java/com/sd/lib/http/body/StringBody.kt
@@ -1,11 +1,10 @@
 package com.sd.lib.http.body
 
-import android.text.TextUtils
 import com.sd.lib.http.ContentType
 
 open class StringBody(content: String?, contentType: String?) : IRequestBody<String> {
 
     override val body: String = content ?: ""
 
-    override val contentType: String = if (TextUtils.isEmpty(contentType)) ContentType.STREAM else contentType!!
+    override val contentType: String = if (contentType == null || contentType.isEmpty()) ContentType.STREAM else contentType
 }
